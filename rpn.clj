b1f43b77d51302a5a1cacabfc311929027cca0e5
François Beausoleil <francois@teksol.info>
2008-12-13 16:16:59 -0500

    REP accross the network

--- rpn.clj
+++ rpn.clj
@@ -7,16 +7,11 @@
   (java.io.OutputStreamWriter. out-stream))
 
 (defn process [socket]
- (let [ in (java.io.LineNumberReader. (make-reader (. socket getInputStream)))
-        line (. in readLine)
-        out (java.io.PrintWriter. (make-writer (. socket getOutputStream)))]
-   (println "line read:\n\t'" line "'")
-   (println "Class of line: " (.. line getClass getName))
-   (. out print "you sent: '")
-   (. out print line)
-   (. out println "'")
-   (. out flush)
-   (. socket close)))
+  (let [ in (java.io.PushbackReader. (make-reader (. socket getInputStream)))
+         out (java.io.PrintWriter. (make-writer (. socket getOutputStream)))]
+    (. out println (eval (read in)))
+    (. out flush)
+    (. socket close)))
 
 (defn run []
   (println "accepting connections")
